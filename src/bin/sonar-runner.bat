58b0d35189cc1926d322119ca7fe4979af3aa895
Evgeny Mandrikov <mandrikov@gmail.com>
2011-02-03 05:58:16 +0000

    SONAR-RUNNER: Fix for Windows script

--- src/bin/sonar-runner.bat
+++ src/bin/sonar-runner.bat
@@ -2,4 +2,4 @@
 
 @echo off
 
-"%JAVA_HOME%\bin\java.exe" -classpath "%SONAR_RUNNER_HOME%\lib\sonar-runner.jar";"%SONAR_RUNNER_HOME%\lib\sonar-batch-bootstrapper.jar" "-Drunner.home=%SONAR_RUNNER_HOME%" org.sonar.runner.Main
+"%JAVA_HOME%\bin\java.exe" -classpath "%SONAR_RUNNER_HOME%\lib\sonar-runner.jar";"%SONAR_RUNNER_HOME%\lib\sonar-batch-bootstrapper.jar" "-Drunner.home=%SONAR_RUNNER_HOME%" org.sonar.runner.Main %*
