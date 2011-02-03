95ce1de831dc7c4e46031364ae6dd9485e977597
Evgeny Mandrikov <mandrikov@gmail.com>
2011-02-03 05:47:06 +0000

    SONAR-RUNNER: Add script for Windows

---
+++ src/bin/sonar-runner.bat
@@ -0,0 +1,5 @@
+@REM Sonar Runner Startup Script for Windows
+
+@echo off
+
+"%JAVA_HOME%\bin\java.exe" -classpath "%SONAR_RUNNER_HOME%\lib\sonar-runner.jar";"%SONAR_RUNNER_HOME%\lib\sonar-batch-bootstrapper.jar" "-Drunner.home=%SONAR_RUNNER_HOME%" org.sonar.runner.Main
--- src/it/example/analyze.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-# TODO remove hard-coded version
-java -jar ../../../target/sonar-cli-0.1-SNAPSHOT.jar project.properties
