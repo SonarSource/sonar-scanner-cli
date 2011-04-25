d9542ebb32efcc3e9ea77e48e40883d4de568915
Evgeny Mandrikov <mandrikov@gmail.com>
2011-04-25 21:22:38 +0000

    SONARPLUGINS-1004 Add support for SONAR_RUNNER_OPTS environment variable

--- src/main/assembly/bin/sonar-runner.bat
+++ src/main/assembly/bin/sonar-runner.bat
@@ -1,5 +1,12 @@
 @REM Sonar Runner Startup Script for Windows
+@REM
+@REM Required ENV vars:
+@REM   JAVA_HOME - location of a JDK home dir
+@REM   SONAR_RUNNER_HOME - location of runner's installed home dir
+@REM
+@REM Optional ENV vars:
+@REM   SONAR_RUNNER_OPTS - parameters passed to the Java VM when running Sonar
 
 @echo off
 
-"%JAVA_HOME%\bin\java.exe" -classpath "%SONAR_RUNNER_HOME%\lib\sonar-runner.jar";"%SONAR_RUNNER_HOME%\lib\sonar-batch-bootstrapper.jar" "-Drunner.home=%SONAR_RUNNER_HOME%" org.sonar.runner.Main %*
+"%JAVA_HOME%\bin\java.exe" %SONAR_RUNNER_OPTS% -classpath "%SONAR_RUNNER_HOME%\lib\sonar-runner.jar";"%SONAR_RUNNER_HOME%\lib\sonar-batch-bootstrapper.jar" "-Drunner.home=%SONAR_RUNNER_HOME%" org.sonar.runner.Main %*
