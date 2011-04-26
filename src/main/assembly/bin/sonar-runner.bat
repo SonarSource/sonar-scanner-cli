1189b4b5f6670267b267ec4c18ac5bae8633b532
Evgeny Mandrikov <mandrikov@gmail.com>
2011-04-26 15:08:37 +0000

    SONARPLUGINS-1114 Automatically determine SONAR_RUNNER_HOME on Windows

--- src/main/assembly/bin/sonar-runner.bat
+++ src/main/assembly/bin/sonar-runner.bat
@@ -2,11 +2,16 @@
 @REM
 @REM Required ENV vars:
 @REM   JAVA_HOME - location of a JDK home dir
-@REM   SONAR_RUNNER_HOME - location of runner's installed home dir
 @REM
 @REM Optional ENV vars:
+@REM   SONAR_RUNNER_HOME - location of runner's installed home dir
 @REM   SONAR_RUNNER_OPTS - parameters passed to the Java VM when running Sonar
 
 @echo off
 
+if NOT "%SONAR_RUNNER_HOME%"=="" goto run
+set SONAR_RUNNER_HOME=%~dp0..
+
+:run
+echo %SONAR_RUNNER_HOME%
 "%JAVA_HOME%\bin\java.exe" %SONAR_RUNNER_OPTS% -classpath "%SONAR_RUNNER_HOME%\lib\sonar-runner.jar";"%SONAR_RUNNER_HOME%\lib\sonar-batch-bootstrapper.jar" "-Drunner.home=%SONAR_RUNNER_HOME%" org.sonar.runner.Main %*
