2bed1e46d1cf5a3e7f0f8936d85432d8d2e5e4bd
Fabrice Bellingard <bellingard@gmail.com>
2012-05-02 11:48:19 +0000

    SONARPLUGINS-1617 Support the properties sonar.showSql and sonar.showSqlResults

--- src/main/assembly/bin/sonar-runner.bat
+++ src/main/assembly/bin/sonar-runner.bat
@@ -9,12 +9,30 @@
 
 @echo off
 
+
+@REM ==== START VALIDATION ====
+if not "%JAVA_HOME%" == "" goto OkJavaHome
+
+echo.
+echo ERROR: JAVA_HOME not found in your environment.
+echo Please set the JAVA_HOME variable in your environment to match the
+echo location of your Java installation
+echo.
+goto end
+
+:OkJavaHome
 if NOT "%SONAR_RUNNER_HOME%"=="" goto run
 set SONAR_RUNNER_HOME=%~dp0..
 
+
+@REM ==== START RUN ====
 :run
 echo %SONAR_RUNNER_HOME%
 
 set PROJECT_HOME=%CD%
 
 "%JAVA_HOME%\bin\java.exe" %SONAR_RUNNER_OPTS% -classpath "%SONAR_RUNNER_HOME%\lib\sonar-runner.jar";"%SONAR_RUNNER_HOME%\lib\sonar-batch-bootstrapper.jar" "-Drunner.home=%SONAR_RUNNER_HOME%" "-Dproject.home=%PROJECT_HOME%" org.sonar.runner.Main %*
+
+
+@REM ==== END EXECUTION ====
+:end
