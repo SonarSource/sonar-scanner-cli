c08690254c76d16e908724858b56c4bfa67b69c5
Simon Brandhof <simon.brandhof@gmail.com>
2013-04-06 19:39:28 +0200

    Add unit test

---
+++ sonar-runner-impl/src/test/java/org/sonar/runner/impl/ServerConnectionTest.java
@@ -0,0 +1,64 @@
+/*
+ * Sonar Runner - Implementation
+ * Copyright (C) 2011 SonarSource
+ * dev@sonar.codehaus.org
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02
+ */
+package org.sonar.runner.impl;
+
+import org.apache.commons.io.FileUtils;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.TemporaryFolder;
+
+import java.io.File;
+import java.util.Properties;
+
+import static org.fest.assertions.Assertions.assertThat;
+
+public class ServerConnectionTest {
+
+  @Rule
+  public MockHttpServerInterceptor httpServer = new MockHttpServerInterceptor();
+
+  @Rule
+  public TemporaryFolder temp = new TemporaryFolder();
+
+  @Test
+  public void should_download_to_string() throws Exception {
+    httpServer.setMockResponseData("abcde");
+    Properties props = new Properties();
+    props.setProperty("sonar.host.url", httpServer.url());
+
+    ServerConnection connection = ServerConnection.create(props);
+    String response = connection.downloadString("/batch/index.txt");
+
+    assertThat(response).isEqualTo("abcde");
+  }
+
+  @Test
+  public void should_download_to_file() throws Exception {
+    httpServer.setMockResponseData("abcde");
+    Properties props = new Properties();
+    props.setProperty("sonar.host.url", httpServer.url());
+
+    ServerConnection connection = ServerConnection.create(props);
+    File toFile = temp.newFile();
+    connection.download("/batch/index.txt", toFile);
+
+    assertThat(FileUtils.readFileToString(toFile)).isEqualTo("abcde");
+  }
+}
