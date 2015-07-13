6a44b5c0095b46414d4c95938a8f5152e14d39aa
David Gageot <david@gageot.net>
2015-07-13 08:57:31 +0200

    Use latest Travis-utils

--- travis.sh
+++ travis.sh
@@ -3,9 +3,9 @@
 set -euo pipefail
 
 function installTravisTools {
-  curl -sSL https://raw.githubusercontent.com/sonarsource/travis-utils/v2.1/install.sh | bash
+  curl -sSL https://raw.githubusercontent.com/sonarsource/travis-utils/v10/install.sh | bash
 }
 
 installTravisTools
-travis_build_green_sonarqube_snapshot
+travis_build_green "SonarSource/sonarqube" "master"
 mvn verify -B -e -V
