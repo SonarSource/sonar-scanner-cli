8f24a793edf5c7e4d8364981a70e25a7161d6fcc
David Gageot <david@gageot.net>
2015-07-20 10:00:46 +0200

    Trying to fix the build on jdk7

--- travis.sh
+++ travis.sh
@@ -3,7 +3,7 @@
 set -euo pipefail
 
 function installTravisTools {
-  curl -sSL https://raw.githubusercontent.com/sonarsource/travis-utils/v11/install.sh | bash
+  curl -sSL https://raw.githubusercontent.com/sonarsource/travis-utils/v12/install.sh | bash
   source /tmp/travis-utils/env.sh
 }
 
@@ -11,18 +11,19 @@ case "$TESTS" in
 
 CI)
   installTravisTools
+
   travis_build_green "SonarSource/sonarqube" "master"
   mvn verify -B -e -V
   ;;
 
 IT-DEV)
-  cat /etc/hosts
-  rpm -qa | grep glibc
   installTravisTools
-  mvn install -Dsource.skip=true -Denforcer.skip=true -Danimal.sniffer.skip=true -Dmaven.test.skip=true
+
   travis_build_green "SonarSource/sonarqube" "master"
+  mvn install -Dsource.skip=true -Denforcer.skip=true -Danimal.sniffer.skip=true -Dmaven.test.skip=true
+
   cd it
-  mvn -Dsonar.runtimeVersion="DEV" -DsonarRunner.version="2.5-SNAPSHOT" -Dmaven.test.redirectTestOutputToFile=false install
+  mvn -DsonarRunner.version="2.5-SNAPSHOT" -Dsonar.runtimeVersion="DEV" -Dmaven.test.redirectTestOutputToFile=false install
   ;;
 
 esac
