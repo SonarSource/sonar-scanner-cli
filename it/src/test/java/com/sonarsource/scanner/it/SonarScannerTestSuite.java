b0583c0fbddafdd013bd9289d0385beb1c299a17
Julien HENRY <julien.henry@sonarsource.com>
2017-07-28 12:36:45 +0200

    SQSCANNER-43 Remove deprecated sonar-runner bat/sh scripts

--- it/src/test/java/com/sonarsource/scanner/it/SonarScannerTestSuite.java
+++ it/src/test/java/com/sonarsource/scanner/it/SonarScannerTestSuite.java
@@ -17,7 +17,7 @@
  * along with this program; if not, write to the Free Software Foundation,
  * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
  */
-package com.sonar.runner.it;
+package com.sonarsource.scanner.it;
 
 import com.sonar.orchestrator.Orchestrator;
 import org.junit.ClassRule;
