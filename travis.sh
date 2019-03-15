f107b55f98cc06e4138a377d6f5a67edbdbc0af1
Simon Brandhof <simon.brandhof@sonarsource.com>
2019-03-15 14:38:43 +0100

    Disable build of branch on Travis if a PR is open

--- travis.sh
+++ travis.sh
@@ -11,5 +11,11 @@ configureTravis
 
 export DEPLOY_PULL_REQUEST=true
 
+# When a pull request is open on the branch, then the job related
+# to the branch does not need to be executed and should be canceled.
+# It does not book slaves for nothing.
+# @TravisCI please provide the feature natively, like at AppVeyor or CircleCI ;-)
+cancel_branch_build_with_pr || exit 0
+
 regular_mvn_build_deploy_analyze -Pdist-linux,dist-windows,dist-macosx
 
