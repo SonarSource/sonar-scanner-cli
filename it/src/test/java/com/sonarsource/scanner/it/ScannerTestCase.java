4bf22616d3b0a5d7a854e25f6f1e7b0b829c9e80
Julien HENRY <julien.henry@sonarsource.com>
2025-01-09 16:46:00 +0100

    Fix ITs now that SQS 2025.1 stopped supporting username/password

--- it/src/test/java/com/sonarsource/scanner/it/ScannerTestCase.java
+++ it/src/test/java/com/sonarsource/scanner/it/ScannerTestCase.java
@@ -116,8 +116,12 @@ public abstract class ScannerTestCase {
   SonarScanner newScannerWithAdminCredentials(File baseDir, String... keyValueProperties) {
     SonarScanner scannerCli = SonarScanner.create(baseDir, keyValueProperties);
     scannerCli.setScannerVersion(artifactVersion().toString());
-    scannerCli.setProperty("sonar.login", Server.ADMIN_LOGIN);
-    scannerCli.setProperty("sonar.password", Server.ADMIN_PASSWORD);
+    if (orchestrator.getServer().version().isGreaterThanOrEquals(25, 1)) {
+      scannerCli.setProperty("sonar.token", orchestrator.getDefaultAdminToken());
+    } else {
+      scannerCli.setProperty("sonar.login", Server.ADMIN_LOGIN);
+      scannerCli.setProperty("sonar.password", Server.ADMIN_PASSWORD);
+    }
     return scannerCli;
   }
 
