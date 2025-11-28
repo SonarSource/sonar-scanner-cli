4df2cbda193ece4ab4c3756440ba0f15537e78a9
Eric Giffon <eric.giffon@sonarsource.com>
2025-11-28 18:27:03 +0100

    NO-JIRA Bump orchestrator version

--- it/src/test/java/com/sonarsource/scanner/it/ScannerTestCase.java
+++ it/src/test/java/com/sonarsource/scanner/it/ScannerTestCase.java
@@ -33,7 +33,7 @@ import java.util.Map;
 import java.util.Properties;
 import java.util.function.Function;
 import java.util.stream.Collectors;
-import org.apache.commons.lang.StringUtils;
+import org.apache.commons.lang3.StringUtils;
 import org.junit.After;
 import org.junit.ClassRule;
 import org.junit.Rule;
