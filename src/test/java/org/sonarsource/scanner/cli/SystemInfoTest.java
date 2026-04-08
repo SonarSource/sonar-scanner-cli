9727df271c075fac355d08fc1948c62163aea6de
Claire Villard <claire.villard@sonarsource.com>
2026-04-08 17:30:47 +0200

    SC-45747 Migrate to dateless license headers

--- src/test/java/org/sonarsource/scanner/cli/SystemInfoTest.java
+++ src/test/java/org/sonarsource/scanner/cli/SystemInfoTest.java
@@ -1,6 +1,6 @@
 /*
  * SonarScanner CLI
- * Copyright (C) 2011-2025 SonarSource Sàrl
+ * Copyright (C) SonarSource Sàrl
  * mailto:info AT sonarsource DOT com
  *
  * This program is free software; you can redistribute it and/or
